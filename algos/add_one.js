2f29b51f46d6396ecfbe9e62b8c9f66e25fe8d6f
JlordA <james.ardery@gmail.com>
2021-02-10 13:56:29 -0500

    charcount

--- algos/add_one.js
+++ algos/add_one.js
@@ -6,4 +6,13 @@ function addUpTo(n) {
     return total;
 }
 
-console.log(addUpTo(6))
+// function addUpTo(n) {
+//     return n * (n + 1) / 2;
+// }
+
+var t1 = performance.now();
+addUpTo(1000000000)
+var t2 = performance.now();
+console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`)
+
+// console.log(addUpTo(6))
